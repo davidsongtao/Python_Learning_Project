92413c87d5b2cb71ab4b4e7cb9af22377276a08b
King Songtao <david.songtao@hotmail.com>
2024-05-09 00:06:34 +0100

    problem sorted by moving to Linux system

--- PySpark框架入门案例/PySpark执行环境入口对象.py
+++ PySpark框架入门案例/PySpark执行环境入口对象.py
@@ -4,9 +4,7 @@
 """
 # 导包
 from pyspark import SparkConf, SparkContext
-import os
 
-os.environ["PYSPARK_PYTHON"] = "D:/Python/python.exe"
 # 创建SparkConf类对象
 conf = SparkConf().setMaster("local[*]").setAppName("test_spark_app")
 
@@ -16,16 +14,11 @@ sc = SparkContext(conf=conf)
 # 准备一个简单的rdd对象
 rdd = sc.parallelize([1, 2, 3, 4, 5])
 
-
 # 基于rdd对象调用.map()运算，把数值都乘10，返回运算结果
-def fun(data):
-    return data * 10
-
-
-rdd2 = rdd.map(fun)
+rdd2 = rdd.map(lambda x: x * 10)
 
 # 打印rdd2
-print(rdd.collect())
+print(rdd2.collect())
 
 # 停止SparkContext对象的运行（停止PySpark程序）
 sc.stop()
