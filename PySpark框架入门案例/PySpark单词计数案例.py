b21207383eac056fe0f58a80ecc7f54d235e321f
King Songtao <david.songtao@hotmail.com>
2024-05-09 18:04:37 +0100

    对代码进行进一步优化，使用map获取最终结果

--- PySpark框架入门案例/PySpark单词计数案例.py
+++ PySpark框架入门案例/PySpark单词计数案例.py
@@ -19,11 +19,11 @@ rdd = sc.textFile("/home/songtao/Documents/hello.txt")
 # 构建二元元组，单词为key,value设置为1 并 通过key对value进行聚合相加计算
 result = rdd.flatMap(lambda x: x.split()).map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
 
-count_python = result.collect()[0][1]  # "python"出现的次数
-count_pyspark = result.collect()[1][1]
-count_itheima = result.collect()[2][1]
-count_spark = result.collect()[3][1]
-count_itcast = result.collect()[4][1]
+count_python = result.map(lambda x: x[1]).collect()[0]
+count_pyspark = result.map(lambda x: x[1]).collect()[1]
+count_itheima = result.map(lambda x: x[1]).collect()[2]
+count_spark = result.map(lambda x: x[1]).collect()[3]
+count_itcast = result.map(lambda x: x[1]).collect()[4]
 
 print(
     f"'python'出现的次数为：{count_python}次。\n'pyspark'出现的次数为:{count_pyspark}次。\n'itheima'出现的次数为:{count_itheima}次。\n'spark'出现的次数为:{count_spark}次。\n'itcast'出现的次数为:{count_itcast}次。")
