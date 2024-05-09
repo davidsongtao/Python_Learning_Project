e538b5ba631c53fd830c48c737bf2f6bfced3bae
King Songtao <david.songtao@hotmail.com>
2024-05-09 15:31:17 +0100

    adddjusted local mysql connection info

--- Python操作MySQL基础使用/pymysql入门.py
+++ Python操作MySQL基础使用/pymysql入门.py
@@ -28,21 +28,21 @@ print(f"版本信息：{connection.get_server_info()}")
 # cursor.execute("create table test_pymysql(id int);")
 
 # 执行查询类的SQL语句，获取查询结果，结果是一个元祖
-cursor = connection.cursor()
-connection.select_db("world")
-cursor.execute("select * from city")
-
-result = cursor.fetchall()  # 通过fetchall()获取查询结果，封装到元祖内
-
-for data in result:  # for循环取出每一条结果数据
-    print(data)
+# cursor = connection.cursor()
+# connection.select_db("world")
+# cursor.execute("select * from city")
+#
+# result = cursor.fetchall()  # 通过fetchall()获取查询结果，封装到元祖内
+#
+# for data in result:  # for循环取出每一条结果数据
+#     print(data)
 
 """
 pymysql在执行数据插入或更改的操作时，默认是需要提交更改的，
     - 通过connection.commit()确认即可
     - 或在构建链接对象的时候设置 autocommit = True，则不需要确认提交
 """
-connection.commit()  # 如果执行插入数据或数据更改，需要执行commit()进行确认
+# connection.commit()  # 如果执行插入数据或数据更改，需要执行commit()进行确认
 
 # 关闭链接
 connection.close()
