9b49a2e5c95882fa6605d66e91fcb9c355a3722e
King Songtao <david.songtao@hotmail.com>
2024-05-09 03:15:22 +0100

    FIXED WARNING

--- PySpark框架入门案例/PySpark执行环境入口对象.py
+++ PySpark框架入门案例/PySpark执行环境入口对象.py
@@ -5,17 +5,19 @@
 # 导包
 from pyspark import SparkConf, SparkContext
 
+
 # 创建SparkConf类对象
 conf = SparkConf().setMaster("local[*]").setAppName("test_spark_app")
 
 # 基于SparkConf类对象创建SparkContext对象
 sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
 
 # 准备一个简单的rdd对象
 rdd = sc.parallelize([1, 2, 3, 4, 5])
 
 # 基于rdd对象调用.map()运算，把数值都乘10，返回运算结果
-rdd2 = rdd.map(lambda x: x * 10)
+rdd2 = rdd.map(lambda x: x * 10).map(lambda x: x + 5)
 
 # 打印rdd2
 print(rdd2.collect())
